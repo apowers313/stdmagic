38cc2c7049b4ab4ab938861ed32515c44167f6df
Adam Powers <apowers@ato.ms>
2020-12-12 09:57:04 -0800

    refactor code

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 module.exports = [
+    ... require("./lib/cellMagics"),
+    ... require("./lib/dir"),
+    ... require("./lib/jupyter"),
     ... require("./lib/node"),
+    ... require("./lib/python"),
+    ... require("./lib/perf"),
     ... require("./lib/shell"),
-    ... require("./lib/stdmagic"),
 ];
+
+if (typeof global.$$ === "object" && typeof global.$$.addMagic === "function") {
+    /* global $$ */
+    module.exports.forEach((v) => {
+        $$.addMagic(v.name, v);
+    });
+}
